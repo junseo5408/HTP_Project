cd0957265df796b880b3a0108a5cecfdb2fd3004
신승호 <tmdgh7273@naver.com>
2024-11-24 15:19:32 +0900

    로르샤흐 테스트 .cs 답안지 채워넣음

--- test_app/Model/RorschachTest.cs
+++ test_app/Model/RorschachTest.cs
@@ -10,16 +10,16 @@
 
         public static IList<RorschachTest> TestItems = new List<RorschachTest>
         {
-            new RorschachTest{ Step = 1, SampleImage = "r1.png", FirstChoice="나비", SecondChoice="박쥐",ThirdChoice ="괴물"},
+            new RorschachTest{ Step = 1, SampleImage = "r1.png", FirstChoice="나비", SecondChoice="박쥐",ThirdChoice ="잭 오 랜턴"},
             new RorschachTest{ Step = 2, SampleImage = "r2.png", FirstChoice="곰", SecondChoice="핏자국",ThirdChoice ="불꽃"},
-            new RorschachTest{ Step = 3, SampleImage = "r3.png", FirstChoice="춤추는 사람", SecondChoice="심장",ThirdChoice =""},
-            new RorschachTest{ Step = 4, SampleImage = "r4.png", FirstChoice="", SecondChoice="",ThirdChoice =""},
-            new RorschachTest{ Step = 5, SampleImage = "r5.png", FirstChoice="", SecondChoice="",ThirdChoice =""},
-            new RorschachTest{ Step = 6, SampleImage = "r6.png", FirstChoice="", SecondChoice="",ThirdChoice =""},
-            new RorschachTest{ Step = 7, SampleImage = "r7.png", FirstChoice="", SecondChoice="",ThirdChoice =""},
-            new RorschachTest{ Step = 8, SampleImage = "r8.png", FirstChoice="", SecondChoice="",ThirdChoice =""},
-            new RorschachTest{ Step = 9, SampleImage = "r9.png", FirstChoice="", SecondChoice="",ThirdChoice =""},
-            new RorschachTest{ Step = 10, SampleImage = "r10.png", FirstChoice="", SecondChoice="",ThirdChoice =""}
+            new RorschachTest{ Step = 3, SampleImage = "r3.png", FirstChoice="두 사람", SecondChoice="심장",ThirdChoice ="가슴"},
+            new RorschachTest{ Step = 4, SampleImage = "r4.png", FirstChoice="부츠", SecondChoice="고릴라",ThirdChoice ="곰"},
+            new RorschachTest{ Step = 5, SampleImage = "r5.png", FirstChoice="나비", SecondChoice="박쥐",ThirdChoice ="움직이는 사람"},
+            new RorschachTest{ Step = 6, SampleImage = "r6.png", FirstChoice="코", SecondChoice="잠수함",ThirdChoice ="동물 가죽"},
+            new RorschachTest{ Step = 7, SampleImage = "r7.png", FirstChoice="두 여자", SecondChoice="뇌운",ThirdChoice ="램프"},
+            new RorschachTest{ Step = 8, SampleImage = "r8.png", FirstChoice="갈비뼈", SecondChoice="동물",ThirdChoice ="나무"},
+            new RorschachTest{ Step = 9, SampleImage = "r9.png", FirstChoice="담배 연기", SecondChoice="꽃",ThirdChoice ="폭발"},
+            new RorschachTest{ Step = 10, SampleImage = "r10.png", FirstChoice="헤엄치는 해저동물들", SecondChoice="불가사리",ThirdChoice ="별"}
         };
 
         public static string[] SampleImagesUrl;
